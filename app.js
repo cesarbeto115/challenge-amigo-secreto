262b89744220c85fd369f57e7eb364af81d420d7
Cesar Serna <cesarbetosc@gmail.com>
2025-02-14 14:27:46 -0600

    Crear el array y funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const regexAmigo = /^[A-Za-záéíóúÁÉÍÓÚñÑ\s]+$/;
+
+function agregarAmigo() {
+    if (amigos.length >= 5) {
+        alert("¡Ya has añadido 5 nombres! No puedes añadir más.");
+        return;
+      }
+
+    let amigo = document.getElementById("nombreAmigo").value;
+
+    if (amigo.trim() === "") {
+      alert("Por favor, ingresa un nombre.");
+      return;
+    }
+
+    //Para saber si el dato ingresado solo sea string
+    if (!regexAmigo.test(amigo)) {
+      alert("Por favor, ingresa un nombre válido");
+      return;
+    }
+
+    amigos.push(amigo);
+
+    // Mostrar el nombre en la página
+    let nuevoAmigo = document.createElement("p");
+    nuevoAmigo.textContent = amigo;
+    document.getElementById("listaAmigos").appendChild(nuevoAmigo);
+
+    limpiarCaja();
+
+    console.log(amigos);
+  }
+
+  function limpiarCaja() {
+    document.getElementById("listaAmigos").value = '';
+}
